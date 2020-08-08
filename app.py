ecb895dc00f346559a6196d8fe8c7bc5993f6fc9
Ederporto <eder.porto@hotmail.com>
2020-08-07 23:46:37 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,218 @@
+import os
+import yaml
+import json
+from requests_oauthlib import OAuth1Session
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify, g
+from flask_babel import Babel
+from oauth_wikidata import get_username, get_token
+from wikidata import query_by_type, query_metadata_of_work, query_depicts_metadata,\
+    api_post_request, post_search_entity, filter_by_tesauros
+
+__dir__ = os.path.dirname(__file__)
+app = Flask(__name__)
+app.config.update(yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+
+BABEL = Babel(app)
+
+
+##############################################################
+# ESTRUTURA
+##############################################################
+@app.before_request
+def init_profile():
+    g.profiling = []
+
+
+@app.before_request
+def global_user():
+    g.user = get_username()
+
+
+@app.route('/login')
+def login():
+    client_key = app.config['CONSUMER_KEY']
+    client_secret = app.config['CONSUMER_SECRET']
+    base_url = 'https://www.wikidata.org/w/index.php'
+    request_token_url = base_url + '?title=Special%3aOAuth%2finitiate'
+
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          callback_uri='oob')
+    fetch_response = oauth.fetch_request_token(request_token_url)
+
+    session['owner_key'] = fetch_response.get('oauth_token')
+    session['owner_secret'] = fetch_response.get('oauth_token_secret')
+
+    base_authorization_url = 'https://www.wikidata.org/wiki/Special:OAuth/authorize'
+    authorization_url = oauth.authorization_url(base_authorization_url,
+                                                oauth_consumer_key=client_key)
+    return redirect(authorization_url)
+
+
+@app.route("/oauth-callback", methods=["GET"])
+def oauth_callback():
+    base_url = 'https://www.wikidata.org/w/index.php'
+    client_key = app.config['CONSUMER_KEY']
+    client_secret = app.config['CONSUMER_SECRET']
+
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=session['owner_key'],
+                          resource_owner_secret=session['owner_secret'])
+
+    oauth_response = oauth.parse_authorization_response(request.url)
+    verifier = oauth_response.get('oauth_verifier')
+    access_token_url = base_url + '?title=Special%3aOAuth%2ftoken'
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=session['owner_key'],
+                          resource_owner_secret=session['owner_secret'],
+                          verifier=verifier)
+
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
+    session['owner_key'] = oauth_tokens.get('oauth_token')
+    session['owner_secret'] = oauth_tokens.get('oauth_token_secret')
+
+    return redirect(url_for("inicio"))
+
+
+# Função para pegar a língua de preferência do usuário
+@BABEL.localeselector
+def get_locale():
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+
+    return session.get('lang', 'pt')
+
+
+# Função para mudar a língua de exibição do conteúdo
+@app.route('/set_locale')
+def set_locale():
+    next_page = request.args.get('return_to')
+    lang = request.args.get('lang')
+
+    session["lang"] = lang
+    return redirect(next_page)
+
+##############################################################
+# PÁGINAS
+##############################################################
+# Página inicial
+@app.route('/')
+@app.route('/home')
+@app.route('/inicio')
+def inicio():
+    username = get_username()
+    return render_template('inicio.html',
+                           username=username,
+                           lang=get_locale())
+
+
+# Página sobre o aplicativo
+@app.route('/about')
+@app.route('/sobre')
+def sobre():
+    username = get_username()
+    return render_template('sobre.html',
+                           username=username,
+                           lang=get_locale())
+
+
+# Página com tutorial de como contribuir com o jogo
+@app.route('/tutorial')
+def tutorial():
+    username = get_username()
+    return render_template('tutorial.html',
+                           username=username,
+                           lang=get_locale())
+
+
+# Página de visualização de coleções definidas por descritores
+@app.route('/colecao/<type>')
+def colecao(type):
+    username = get_username()
+    with open(os.path.join(app.static_folder, 'queries.json')) as category_queries:
+        all_queries = json.load(category_queries)
+
+    try:
+        selected_query = all_queries[type]["query"]
+        selection = query_by_type(selected_query)
+        return render_template("colecao.html",
+                               collection=selection,
+                               username=username,
+                               lang=get_locale())
+    except:
+        return redirect(url_for('inicio'))
+
+
+# Página de visualização da obra e inserção de qualificadores
+@app.route('/item/<qid>')
+def item(qid):
+    username = get_username()
+    with open(os.path.join(app.static_folder, 'queries.json')) as category_queries:
+        all_queries = json.load(category_queries)
+
+    metadata_query = all_queries["Metadados"]["query"].replace("LANGUAGE", get_locale()).replace("QIDDAOBRA", qid)
+    depicts_query = all_queries["Retratas"]["query"].replace("LANGUAGE", get_locale()).replace("QIDDAOBRA", qid)
+    work_metadata = query_metadata_of_work(metadata_query, lang=get_locale)
+    work_depicts = query_depicts_metadata(depicts_query, qid)
+
+    return render_template('item.html',
+                           metadata=work_metadata,
+                           depicts_metadata=work_depicts,
+                           username=username,
+                           lang=get_locale())
+
+
+##############################################################
+# CONSULTAS E REQUISIÇÕES
+##############################################################
+# Requisição de adicionar qualificador à item retratado
+@app.route('/add_stat', methods=['GET', 'POST'])
+def add_statement():
+    if request.method == 'POST':
+        data = request.get_json()
+        statement = data['statid'].split("#")[-1]  # Identificador da declaração
+        pid = data['tipo']  # Tipo de qualificador
+        qual = data['qual']  # QID do qualificador a ser adicionado
+
+        token = get_token()
+        params = {
+            "action": "wbsetqualifier",
+            "format": "format",
+            "claim": statement,
+            "property": pid,
+            "value": "{\"entity-type\":\"item\",\"numeric-id\":" + str(qual) + "}",
+            "snaktype": "value",
+            "token": token
+        }
+
+        results = api_post_request(params)
+
+        return jsonify(results=results)
+    return render_template('item.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_entity():
+    if request.method == "POST":
+        data = request.get_json()
+        term = data['term']
+        lang = get_locale()
+
+        data = post_search_entity(term, lang)
+
+        items = []
+        for item in data["search"]:
+            items.append(item["id"])
+
+        query = filter_by_tesauros("wd:"+" wd:".join(items))
+
+        if query:
+            return jsonify(query), 200
+        else:
+            return jsonify(query), 204
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ wikidata.py
@@ -0,0 +1,138 @@
+import requests
+import roman
+import math
+import re
+from flask import current_app, session
+from requests_oauthlib import OAuth1Session
+
+
+##############################################################
+# QUERIES
+##############################################################
+def query_wikidata(query):
+    url = "https://query.wikidata.org/sparql"
+    params = {
+        "query": query,
+        "format": "json"
+    }
+    result = requests.get(url=url, params=params, headers={'User-agent': 'Trajo 1.0'})
+    data = result.json()
+    return data
+
+
+def query_by_type(query, lang="pt-br"):
+    new_query = query.replace("LANGUAGE", lang)
+    data = query_wikidata(new_query)
+    result = data["results"]["bindings"]
+
+    images = []
+    for image in result:
+        images.append({
+            "qid": image["item_qid"]["value"],
+            "label": image["item_label"]["value"],
+            "imagem": image["imagem"]["value"]
+        })
+
+    return images
+
+
+def query_metadata_of_work(query, lang="pt-br"):
+    data = query_wikidata(query)
+    result = data["results"]["bindings"][0]
+    format_dates_in_result(result, lang)
+    get_values_lists(result)
+    if result == [{}]:
+        result = ""
+    return result
+
+
+def query_depicts_metadata(query, qid):
+    data = query_wikidata(query)
+    result = data["results"]["bindings"]
+    for depicted_entity in result:
+        get_values_lists(depicted_entity, sep=";%;")
+        if 'retrata_stat_id' in depicted_entity:
+            depicted_entity['retrata_stat_id'][0] = "https://www.wikidata.org/wiki/"+qid+"#"+depicted_entity['retrata_stat_id'][0].replace('-', '$', 1)
+    return result
+
+
+def format_dates_in_result(result, lang="pt-br"):
+    if "data" in result:
+        datas = result["data"]["value"].split(";")
+        novas_datas = []
+        for data in datas:
+            novas_datas.append(format_dates(data, lang))
+        result["data"]["value"] = ";".join(novas_datas)
+
+
+def format_dates(time, lang="pt-br"):
+    year, month, day, precision = list(map(int, re.findall(r'\d+', time)))
+    if precision == 7:
+        if lang == "en":
+            date = "%dth century" % int(year/1000+1)
+        else:
+            if year % 1000 == 0:
+                date = "Século %s" % roman.toRoman(year/100)
+            else:
+                date = "Século %s" % roman.toRoman(math.floor(year / 100) + 1)
+    elif precision == 8:
+        if lang == "en":
+            date = "%ds" % int(year/10)*10
+        else:
+            date = "Década de %d" % int(year/10)*10
+    elif precision == 9:
+        date = "%d" % year
+    elif precision == 10:
+        date = "%d/%d" % (month, year)
+    elif precision == 11:
+        if lang == "en":
+            date = "%d/%d/%d" % (month, day, year)
+        else:
+            date = "%d/%d/%d" % (day, month, year)
+    else:
+        date = ""
+    return date
+
+
+def get_values_lists(result, sep=";"):
+    for metadata_key, metadata_dict in result.items():
+        result[metadata_key] = list(filter(None, metadata_dict["value"].split(sep)))
+
+
+def api_post_request(params):
+    app = current_app
+    url = 'https://www.wikidata.org/w/api.php'
+    client_key = app.config['CONSUMER_KEY']
+    client_secret = app.config['CONSUMER_SECRET']
+    oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=session['owner_key'],
+                          resource_owner_secret=session['owner_secret'])
+    return oauth.post(url, data=params, timeout=4)
+
+
+def post_search_entity(term, lang="pt-br"):
+    url = 'https://www.wikidata.org/w/api.php'
+    params = {
+        'action': 'wbsearchentities',
+        'search': term,
+        'language': lang,
+        'format': 'json',
+        'limit': 50,
+        'uselang': lang,
+    }
+    result = requests.get(url=url, params=params, headers={'User-agent': 'Trajo 1.0'})
+    data = result.json()
+
+    return data
+
+
+def filter_by_tesauros(qids, lang="pt-br"):
+    data = query_wikidata("SELECT DISTINCT ?item_qid ?item_label ?item_descr WHERE { SERVICE wikibase:label {bd:serviceParam wikibase:language '"+lang+",pt-br,pt,en,es,fr,de'. ?item rdfs:label ?item_label. ?item schema:description ?item_descr.} VALUES ?item {"+qids+"} VALUES ?propriedade {wdt:P8514 wdt:P3832 wdt:P1014 wdt:P1256 wdt:P7749} ?item ?propriedade []. BIND(SUBSTR(STR(?item),32) AS ?item_qid) }")
+    results = data["results"]["bindings"]
+    query = []
+    for item in results:
+        query.append({"qid": item["item_qid"]["value"],
+                      "label": item["item_label"]["value"],
+                      "descr": item["item_descr"]["value"]})
+    return query
